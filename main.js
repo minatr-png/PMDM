3dad2ad3d9446da858dd4866a6a5995aa3ae2036
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2020-09-24 17:52:27 +0200

    AE1-1 hecho

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 //Part A
-console.log("Soy el primer usuario");
+alert("Soy el primer usuario");
+
+//Part B
+let alertMessage = alert("Soy el primer script");
+
+//Part C
+const ALERT_MESSAGE = alert(alertMessage,"y estoy funcionando sobre",navigator.appVersion);
