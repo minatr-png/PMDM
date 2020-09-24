d576cd6f75a0486302db7856a3079cef385c244f
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2020-09-24 17:56:43 +0200

    AE1-2 hecha

---
+++ ej2.js
@@ -0,0 +1,9 @@
+let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+//Part A
+months.forEach(element => {
+    console.log(element);
+});
+
+//Part B
+console.table(months);
