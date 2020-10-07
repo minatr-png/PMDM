2243e18b6f66158000e4a5ba840a6d8a2dbb5030
minatr-png <57366379+minatr-png@users.noreply.github.com>
2020-10-07 12:34:09 +0200

    Delete ej2.js

--- ej2.js
+++
@@ -1,9 +0,0 @@
-let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
-//Part A
-months.forEach(element => {
-    console.log(element);
-});
-
-//Part B
-console.table(months);
