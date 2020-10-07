bc47dcf41088b8801a7469a76ca25a31d1ac5aa1
minatr-png <57366379+minatr-png@users.noreply.github.com>
2020-10-07 12:35:22 +0200

    Delete ej6.js

--- ej6.js
+++
@@ -1,7 +0,0 @@
-//Para este ejercicio he instalado readline-sync
-input = require('readline-sync');
-
-let num = input.question('Dime un numero: ');
-
-if (num%2 == 0) console.log("El número es par.");
-else console.log("El número es impar.");
