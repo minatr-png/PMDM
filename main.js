5d551aaa7fcf7bc2bcc6b2cc17517d9f83397f04
minatr-png <57366379+minatr-png@users.noreply.github.com>
2020-10-07 12:35:48 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-//Part A
-alert("Soy el primer usuario");
-
-//Part B
-let alertMessage = alert("Soy el primer script");
-
-//Part C
-const ALERT_MESSAGE = alert(alertMessage,"y estoy funcionando sobre",navigator.appVersion);
