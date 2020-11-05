ee23a05d2c96c91f10edd5ec593426a8e14565f2
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2020-11-05 12:44:23 +0100

    Comillas simples y mayusculas automáticas añadidas

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const bac3 = document.getElementById('back3');
     const fini = document.getElementById('finish');
     const urlI = document.getElementById('url');
+    const towM = document.getElementById('town');
     
     stepManager();
 
@@ -22,6 +23,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         else step2ContinueAbled(cont);
     });
+    back.addEventListener('click', () =>
+    {
+        stepBack();
+    });
     con3.addEventListener('click', () =>
     {
         nextStep();
@@ -30,13 +35,13 @@ document.addEventListener('DOMContentLoaded', () => {
     {
         stepBack();
     });
-    back.addEventListener('click', () =>
+    towM.addEventListener('focusout', () => 
     {
-        stepBack();
+       towM.value = towM.value.toUpperCase();
     });
     fini.addEventListener('click', () => 
     {
-        location.replace(document.getElementById("url").value);
+        location.replace(document.getElementById('url').value);
     });
     urlI.addEventListener('input', () =>
     {
@@ -45,61 +50,61 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const step1ContinueAbled = () => {
-    if (document.getElementById("url").value != "") document.getElementById("continue").disabled = false;
-    else document.getElementById("continue").disabled = true;
+    if (document.getElementById('url').value != '') document.getElementById('continue').disabled = false;
+    else document.getElementById('continue').disabled = true;
 }
 
 const step2ContinueAbled = (cont) => {
-    var errors   = document.getElementById("errors");
-    var erName   = document.getElementById("erName");
-    var erBirth  = document.getElementById("erBirth");
-    var erTown   = document.getElementById("erTown");
-    var erPostal = document.getElementById("erPostal");
+    var errors   = document.getElementById('errors');
+    var erName   = document.getElementById('erName');
+    var erBirth  = document.getElementById('erBirth');
+    var erTown   = document.getElementById('erTown');
+    var erPostal = document.getElementById('erPostal');
 
-    var nameTF = document.getElementById("name").value      == "";
-    var birtTF = document.getElementById("birthDate").value == "";
-    var townTF = document.getElementById("town").value      == "";
-    var postTF = document.getElementById("postal").value;
+    var nameTF = document.getElementById('name').value      == '';
+    var birtTF = document.getElementById('birthDate').value == '';
+    var townTF = document.getElementById('town').value      == '';
+    var postTF = document.getElementById('postal').value;
 
-    if (nameTF || birtTF || postTF === "" || postTF.length > 4 || townTF)
+    if (nameTF || birtTF || postTF === '' || postTF.length > 4 || townTF)
     {
         var errorsText = [];
 
         if (nameTF) 
         {
-            erName.style.display  = "";
+            erName.style.display  = '';
             errorsText.push(erName.textContent);
         }
-        else erName.style.display = "none";
+        else erName.style.display = 'none';
         if (birtTF) 
         {
-            erBirth.style.display = "";
+            erBirth.style.display = '';
             errorsText.push(erBirth.textContent);
         }
-        else erBirth.style.display = "none";
+        else erBirth.style.display = 'none';
         if (townTF) 
         {
-            erTown.style.display  = "";
+            erTown.style.display  = '';
             errorsText.push(erTown.textContent);
         }
-        else erTown.style.display = "none";
-        if (postTF === "" || postTF.length > 4) 
+        else erTown.style.display = 'none';
+        if (postTF === '' || postTF.length > 4) 
         {
-            erPostal.style.display   = "";
+            erPostal.style.display   = '';
             errorsText.push(erPostal.textContent);
         }
-        else erPostal.style.display = "none";
+        else erPostal.style.display = 'none';
 
         while (errors.childElementCount != 0) errors.removeChild(errors.lastChild);
 
         errorsText.forEach(element => {
-            var li   = document.createElement("LI");
+            var li   = document.createElement('LI');
             var text = document.createTextNode(element);
             li.appendChild(text);
-            document.getElementById("errors").appendChild(li);
+            document.getElementById('errors').appendChild(li);
         });
 
-        errors.style.display = "";
+        errors.style.display = '';
     }
     else
     {
@@ -122,59 +127,59 @@ const dataObj = () => {
     while (ul.childElementCount != 0) ul.removeChild(ul.lastChild);
 
     Object.entries(user).forEach(element => {
-        var li   = document.createElement("LI");
-        var text = document.createTextNode(element[0] + ": " + element[1]);
+        var li   = document.createElement('LI');
+        var text = document.createTextNode(element[0] + ': ' + element[1]);
         li.appendChild(text);
-        document.getElementById("list").appendChild(li);
+        document.getElementById('list').appendChild(li);
     });
 }
 
 const stepManager = () => {
     if (step == 1)
     {
-        document.getElementById("step2").style.display = "none";
-        document.getElementById("step3").style.display = "none";
-        document.getElementById("step4").style.display = "none";
-        document.getElementById("back").style.display  = "none";
+        document.getElementById('step2').style.display = 'none';
+        document.getElementById('step3').style.display = 'none';
+        document.getElementById('step4').style.display = 'none';
+        document.getElementById('back').style.display  = 'none';
 
-        document.getElementById("step1").style.display    = "";
-        document.getElementById("continue").style.display = "";
+        document.getElementById('step1').style.display    = '';
+        document.getElementById('continue').style.display = '';
 
         step1ContinueAbled();
     }
     if (step == 2)
     {
-        document.getElementById("step1").style.display    = "none";
-        document.getElementById("step3").style.display    = "none";
-        document.getElementById("step4").style.display    = "none";
-        document.getElementById("errors").style.display   = "none";
-        document.getElementById("erName").style.display   = "none";
-        document.getElementById("erBirth").style.display  = "none";
-        document.getElementById("erPostal").style.display = "none";
-        document.getElementById("erTown").style.display   = "none";
+        document.getElementById('step1').style.display    = 'none';
+        document.getElementById('step3').style.display    = 'none';
+        document.getElementById('step4').style.display    = 'none';
+        document.getElementById('errors').style.display   = 'none';
+        document.getElementById('erName').style.display   = 'none';
+        document.getElementById('erBirth').style.display  = 'none';
+        document.getElementById('erPostal').style.display = 'none';
+        document.getElementById('erTown').style.display   = 'none';
 
-        document.getElementById("step2").style.display = "";
-        document.getElementById("back").style.display  = "";
+        document.getElementById('step2').style.display = '';
+        document.getElementById('back').style.display  = '';
     }
     if (step == 3)
     {
-        document.getElementById("step1").style.display = "none";
-        document.getElementById("step2").style.display = "none";
-        document.getElementById("step4").style.display = "none";
+        document.getElementById('step1').style.display = 'none';
+        document.getElementById('step2').style.display = 'none';
+        document.getElementById('step4').style.display = 'none';
 
-        document.getElementById("step3").style.display    = "";
-        document.getElementById("back").style.display     = "";
-        document.getElementById("continue").style.display = "";
+        document.getElementById('step3').style.display    = '';
+        document.getElementById('back').style.display     = '';
+        document.getElementById('continue').style.display = '';
     }
     if (step == 4)
     {
-        document.getElementById("step1").style.display    = "none";
-        document.getElementById("step2").style.display    = "none";
-        document.getElementById("step3").style.display    = "none";
-        document.getElementById("continue").style.display = "none";
+        document.getElementById('step1').style.display    = 'none';
+        document.getElementById('step2').style.display    = 'none';
+        document.getElementById('step3').style.display    = 'none';
+        document.getElementById('continue').style.display = 'none';
 
-        document.getElementById("step4").style.display = "";
-        document.getElementById("back").style.display  = "";
+        document.getElementById('step4').style.display = '';
+        document.getElementById('back').style.display  = '';
     }
 }
 
