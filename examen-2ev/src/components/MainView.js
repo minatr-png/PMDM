685d3036617c448adf7f70a2ab248a1a50eeb0cb
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2021-01-29 16:53:23 +0100

    (El anterior commit era del ejercicio 5) Ejercicio 3 hecho

--- examen-2ev/src/components/MainView.js
+++ examen-2ev/src/components/MainView.js
@@ -3,15 +3,25 @@ import { NavLink } from 'react-router-dom';
 import './EXSecondView.css';
 
 class MainView extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            title: "Welcome to the IT world"
+        }
+    }
+    
+    componentDidMount() {
+        this.setState({title: "Already in!"});
+        console.log("Change for exercise 3, done");
+    }
+    
     render() {
         return (
-            <div>                
-                <h1>Welcome to the first view!</h1>
-                <h2>Well done!</h2>
+            <div>
+                <h1>{this.state.title}</h1>
                 <NavLink to={'/EXSeconView'}><button>Ir a la segunda página</button></NavLink>
                 <p></p>
-                <NavLink to={'/Ejercicio1'}><button>Ir al ejercicio 1</button></NavLink>
+                <NavLink to={'/Ejercicio1'}><button>Ir al ejercicio 1 y 5</button></NavLink>
             </div>
         )
     }
