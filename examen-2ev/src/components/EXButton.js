73fee2f7ab9f1fd7ae9a896762ba253152355537
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2021-01-29 15:48:32 +0100

    Ejercicio 1 hecho

---
+++ examen-2ev/src/components/EXButton.js
@@ -0,0 +1,7 @@
+import './EXButton.css';
+
+export const EXButton = (props) => {
+    return (
+        <button type={'submit'}>Click me!</button>
+    );
+}
---
+++ examen-2ev/src/components/EXSecondView.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import './EXSecondView.css'
+
+class EXSecondView extends React.Component{
+
+    render() {
+        return(
+            <div className={'second-view-container'}>
+                <h1>Welcome to the second view!</h1>
+                <h2>Well done!</h2>
+            </div>
+        )
+    }
+
+}
+
+export default EXSecondView;
