acc76a272199a83eabdf58ee366481b3612cd597
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2021-01-29 16:12:48 +0100

    Botón alineado verticalmente

--- examen-2ev/src/App.js
+++ examen-2ev/src/App.js
@@ -3,8 +3,10 @@ import EXButton from './components/EXButton';
 
 function App() {
   return (
-    <div className="App" style={{ flex: 1, alignContent: "center", justifyContent: "center", backgroundColor: 'red' }}>
-      <EXButton text='Listo para hacer click'></EXButton>
+    <div className="App">
+      <div style={{alignSelf: 'center', justifyContent: 'center'}}>
+        <EXButton text='Listo para hacer click' style={{height: 100}}></EXButton>
+      </div>
     </div>
   );
 }
